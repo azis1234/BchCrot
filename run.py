e79d130382fa4155bea86870e1f611e17b53d4b6
Azis18 <44446834+azis1234@users.noreply.github.com>
2019-03-16 04:50:21 +0700

    run.py

--- run.py
+++ run.py
@@ -1,4 +1,14 @@
-#Compiled By xNot_Found
-#Github : https://github.com/hatakecnk
-import marshal
-exec(marshal.loads('c\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x00\x00@\x00\x00\x00s\x1d\x00\x00\x00d\x00\x00d\x01\x00l\x00\x00Z\x00\x00e\x00\x00j\x01\x00d\x02\x00\x83\x01\x00\x01d\x01\x00S(\x03\x00\x00\x00i\xff\xff\xff\xffNs\x19\x00\x00\x00cd .core && python run.py(\x02\x00\x00\x00t\x02\x00\x00\x00ost\x06\x00\x00\x00system(\x00\x00\x00\x00(\x00\x00\x00\x00(\x00\x00\x00\x00s\x07\x00\x00\x00<debby>t\x08\x00\x00\x00 '))
+#!/bin/usr/python2
+#Mau Recode Ya Bangsat?
+#Dasar Otak Udang!
+import os
+import sys
+os.system('clear')
+print ('\n\n')
+print ('\033[1;96m ╦ ╦╔═╗╦  ╔═╗╔═╗╔╦╗╔═╗  ╦ ╦╔═╗╔═╗╦═╗')
+print (' ║║║║╣ ║  ║  ║ ║║║║║╣   ║ ║╚═╗║╣ ╠╦╝')
+print (' ╚╩╝╚═╝╩═╝╚═╝╚═╝╩ ╩╚═╝  ╚═╝╚═╝╚═╝╩╚═')
+print ('\033[0;90m    [\033[1;36m+\033[0;90m] Enter your phone_number')
+print ('\033[0;90m    [\033[1;36m+\033[0;90m] Example: +6283195766895')
+doge = input('\033[0;90m    [\033[1;36m+\033[0;90m] Your Number: \033[1;36m')
+os.system('python main.py ' + doge)
